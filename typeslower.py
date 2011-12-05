0374c279f7b36161e195ae4e153f9c2396af9e5e
Rory McCann <rory@technomancy.org>
2011-12-05 10:28:45 +0000

    add sample app indicator

--- typeslower.py
+++ typeslower.py
@@ -3,15 +3,21 @@ from __future__ import division
 
 import time, datetime, pickle, signal
 import pyxhook
+import pynotify
 from threading import Thread, Timer
 
+import gobject, gtk, appindicator
+
 TOO_FAST = [
-    {'chars':10, 'sec':5}
+    {'chars':10, 'sec':5},
+    #{'chars':200, 'sec':60},
 ]
 
 
 class TypeSlowerMonitor(object):
-    def __init__(self):
+    def __init__(self, indicator):
+        self.indicator = indicator
+
         hm = pyxhook.HookManager()
         hm.HookKeyboard()
         hm.HookMouse()
@@ -24,8 +30,8 @@ class TypeSlowerMonitor(object):
 
         self.keypresses = []
 
-        #self.status_printer = StatusPrinter(self)
-        #self.status_printer.start()
+        self.status_printer = StatusChecker(self, indicator)
+        self.status_printer.start()
 
         self.dump()
         hm.join()
@@ -47,27 +53,93 @@ class TypeSlowerMonitor(object):
         
 
 
-class StatusPrinter(Thread):
-    def __init__(self, monitor):
+class StatusChecker(Thread):
+    def __init__(self, monitor, indicator):
         Thread.__init__(self)
         self.monitor = monitor
+        self.indicator = indicator
 
     def run(self):
         while True:
             now = datetime.datetime.now()
             now_time = time.time()
-            result = "%s " % now
+            overs = None
+            fmt = " {: 2d} of {: 2d} ({: 1.1f})"
+            #fmt = " {: 2d} of {: 2d} ({: 1.1f})"
+            guide = fmt.format(0, 0, 0, 0) * len(TOO_FAST)
+            result = ""
             for level in TOO_FAST:
                 chars, sec = level['chars'], level['sec']
                 num = len([x for x in self.monitor.keypresses if (now_time - x) <= sec])
-                result += " %d of %d (%.1f) in %ds" % (num, chars, (num/chars), sec)
+                result += fmt.format(num, chars, (num/chars), sec)
 
-            #print result
-            time.sleep(1 - (datetime.datetime.now().microsecond / 1000000))
+            self.indicator.set_label(result)
+            time.sleep(0.5)
     
 
-def main():
-    monitor = TypeSlowerMonitor()
+#def main():
+#    monitor = TypeSlowerMonitor()
+#
+
+class TypeSlowerIndicator(object):
+    def d__init__(self):
+
+        ind = appindicator.Indicator ("example-simple-client", "indicator-messages", appindicator.CATEGORY_APPLICATION_STATUS)
+
+        #ind = appindicator.Indicator ("indicator-sysmonitor",
+        #  "sysmonitor",
+        #    appindicator.CATEGORY_SYSTEM_SERVICES)
+        ind.set_status(appindicator.STATUS_ACTIVE)
+        ind.set_attention_icon("indicator-messages-new")
+        #ind.set_label("init")
+
+        menu_item = gtk.MenuItem("test")
+        menu = gtk.Menu()
+        menu.append(menu_item)
+        menu_item.show()
+        ind.set_menu(menu)
+
+        self.ind = ind
+
+    def __init__(self):
+        ind = appindicator.Indicator ("example-simple-client", "indicator-messages", appindicator.CATEGORY_APPLICATION_STATUS)
+        ind.set_status (appindicator.STATUS_ACTIVE)
+        ind.set_attention_icon ("indicator-messages-new")
+
+        # create a menu
+        menu = gtk.Menu()
+
+        # create some labels
+        for i in range(3):
+            buf = "Test-undermenu - %d" % i
+
+            menu_items = gtk.MenuItem(buf)
+
+            menu.append(menu_items)
+
+            # this is where you would connect your menu item up with a function:
+
+            # menu_items.connect("activate", self.menuitem_response, buf)
+
+            # show the items
+            menu_items.show()
+
+        ind.set_menu(menu)
+        self.ind = ind
+
+    def set_label(self, text):
+        print text
+        #self.ind.set_label(text, text)
+        
+
+
+if __name__ == "__main__":
+    indicator = TypeSlowerIndicator()
+    # commenting this out makes the indicator not work.
+    # look at how indicator-sysmonitor does it
+    monitor = TypeSlowerMonitor(indicator)
+
+    gtk.main()
 
 if __name__ == '__main__':
     main()
