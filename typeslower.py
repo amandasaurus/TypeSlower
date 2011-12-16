802755d05a777ff8a5c9305c4ee96f906644af9a
Rory McCann <rory@technomancy.org>
2011-12-16 15:05:50 +0000

    basic partially working notify-osd set up

--- typeslower.py
+++ typeslower.py
@@ -4,7 +4,12 @@ from __future__ import division
 
 import time, datetime, pickle, signal
 import pyxhook
+import math
+
+import pygtk
+pygtk.require('2.0')
 import pynotify
+
 from threading import Thread, Timer, Event
 
 import gobject, gtk, appindicator
@@ -23,8 +28,10 @@ class UpdateLabel(Thread):
     def __init__(self, indicator):
         Thread.__init__(self)
         self.indicator = indicator
+        self.notifications = {}
 
     def run(self):
+        INTERVAL = 0.2
         while not self.indicator.finished.is_set():
             now = time.time()
             new_label_parts = []
@@ -33,8 +40,22 @@ class UpdateLabel(Thread):
                 ratio = num_this_period / period['chars']
                 new_label_parts.append("{ratio:.1f} ({time})".format(time=period['sec'], ratio=ratio))
 
+                if ratio > 1:
+                    warning_string = "{num} keypresses in {time} sec, Max: {max}, ×{ratio:.1f} safe".format(num=num_this_period, time=period['sec'], ratio=ratio, max=period['chars'])
+                    notif = self.notifications.get(period['sec'], pynotify.Notification("Slow Down!", warning_string))
+                    notif.update("Slow Down!", warning_string)
+                    notif.show()
+                    self.notifications[period['sec']] = notif
+                else:
+                    if period['sec'] in self.notifications:
+                        print "want to close"
+                        self.notifications[period['sec']].close()
+                        del self.notifications[period['sec']]
+
+
+
             self.indicator.ind.set_label(" ".join(new_label_parts))
-            time.sleep(0.2)
+            time.sleep(INTERVAL)
 
 
 class TypeSlowerIndicator(object):
@@ -88,6 +109,8 @@ if __name__ == "__main__":
     indicator = TypeSlowerIndicator()
 
     try:
+        if not pynotify.init("TypeSlower"):
+            sys.exit(1)
         gtk.main()
     finally:
         indicator.cancel()
