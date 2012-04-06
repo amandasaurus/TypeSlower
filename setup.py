56d104da56c14d48598810b9890033ee79bdeb38
Rory McCann <rory@technomancy.org>
2012-04-06 16:04:19 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="typeslower",
+      version="0.1",
+      author="Rory McCann",
+      author_email="rory@technomancy.org",
+      packages=['typeslower'],
+)
